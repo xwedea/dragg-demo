8a002e2d00b5ed8e977d85cf3e70559017d6765f
xwedea <mecitjiyan@gmail.com>
2024-01-09 11:16:07 -0800

    button clicked textures

---
+++ src/coin.gd
@@ -0,0 +1,10 @@
+class_name Coin extends Pullable
+
+func _ready():
+	super()
+	print("readycoin")
+	# apply_impulse(Vector3.UP * 5)
+
+
+
+
--- src/pullable.gd
+++ src/pullable.gd
@@ -9,12 +9,14 @@ var collect_audio: AudioStreamPlayer
 func _ready():
 	world = get_tree().root.get_node("World3D") as Node3D
 	player = world.get_node("Player") as BaseCharacter
-	collect_audio = get_node("CollectAudio")
+	collect_audio = get_node("CollectAudio") as AudioStreamPlayer
+
 
 func _process(delta):
 	if getting_pulled:
 		var weight = 10 * delta
 		position = position.move_toward(player.position, weight)
+
 
 func start_pulling():
 	getting_pulled = true
