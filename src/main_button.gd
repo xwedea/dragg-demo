8fb85a95ea2a8db60097195d8ac3c9ee289442b2
xwedea <mecitjiyan@gmail.com>
2024-01-11 18:10:26 -0500

    modify buttons

--- src/main_button.gd
+++ src/main_button.gd
@@ -1,11 +1,13 @@
-class_name MainButton extends TextureButton
+class_name ButtonControl extends Control
 
-var tap_audio: AudioStreamPlayer
+@onready var tap_audio := get_node("TapAudio") as AudioStreamPlayer
+@onready var button := get_node("Button") as Button 
 
 
 func _ready():
-	tap_audio = get_node("TapAudio")
+	pass
 
 
 func _on_button_down():
+	scale = Vector2(0.9, 0.9)
 	tap_audio.play()
