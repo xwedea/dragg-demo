0eb2c1bbe1a644f10fc99f16cdf7695e84d2f290
xwedea <mecitjiyan@gmail.com>
2024-01-11 19:55:59 -0500

    new title screen

--- src/button_control.gd
+++ src/button_control.gd
@@ -12,6 +12,5 @@ func _on_texture_button_button_down():
 	tap_audio.play()
 
 
-
 func _on_texture_button_button_up():
 	scale = Vector2(1, 1)
--- src/title_control.gd
+++ src/title_control.gd
@@ -1,16 +1,12 @@
 extends Control
 
 @onready var play_button := get_node("PlayButton")
-@onready var tutorial_button := get_node("TutorialButton")
-@onready var title_audio := get_node("TitleAudio")
 
-# var casual_font_res := preload("res://ui/fonts/supercell-magic-webfont.ttf") as Font
-
-func _ready():
-	title_audio.play()
-	# add_theme_font_override("casual", casual_font_res)
 
 func _on_play_button_pressed():
 	get_tree().change_scene_to_file("res://world/development_world.tscn")
 
 
+
+func _on_texture_button_pressed():
+	pass # Replace with function body.
