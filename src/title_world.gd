8b20064415dc6021c7e080f8385f3308db34f485
Jiyan Ayhan <mecitjiyan@gmail.com>
2024-09-26 14:18:48 -0400

    sign in test

--- src/title_world.gd
+++ src/title_world.gd
@@ -3,15 +3,30 @@ extends Node3D
 @onready var background_audio := get_node("BackgroundAudio") as AudioStreamPlayer
 @onready var signin_label := get_node("UI/SignInLabel") as Label
 
+var _sign_in_retries := 5
+
 func _ready():
 	if background_audio:
 		background_audio.play()
 
-	print("title ready()")
-	SignInClient.user_authenticated.connect(_on_user_authenticated)
+	if not GodotPlayGameServices.android_plugin:
+		signin_label.text = "Plugin Not Found!"
+	
+	SignInClient.user_authenticated.connect(func(is_authenticated: bool):
+		if _sign_in_retries > 0 and not is_authenticated:
+			signin_label.text = "Trying to sign in!"
+			SignInClient.sign_in()
+			_sign_in_retries -= 1
+		
+		if _sign_in_retries == 0:
+			signin_label.text = "Sign in attemps expired!"
+		
+		if is_authenticated:
+			signin_label.text = "Authenticated!"
+	)
 
 func _on_user_authenticated(is_authenticated):
-	signin_label.text = "is authenticated: " + str(is_authenticated)
+	_update_signin_label("is authenticated: " + str(is_authenticated))
 
 	if not is_authenticated:
 		SignInClient.sign_in()
@@ -19,6 +34,9 @@ func _on_user_authenticated(is_authenticated):
 
 func _on_user_signed_in():
 	print("signed in")
+
+func _update_signin_label(text):
+	signin_label.text = text
 
 
 
