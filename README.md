1c084709a795c85b037cca308eda5d12d3d0dbd9
Jiyan Ayhan <mecitjiyan@gmail.com>
2024-12-22 16:31:19 -0500

    add map and readme.md

---
+++ README.md
@@ -0,0 +1,8 @@
+# Dragg Demo
+
+Dragg is a 3D rogue-lite survival game, designed to be played with one finger.
+
+This demo includes the game mechanics and google play account integration.
+
+# How to Use
+Clone the repository and run project.godot with Godot 4.3+
--- src/title_ui.gd
+++ src/title_ui.gd
@@ -6,5 +6,5 @@ func _ready():
 	play_button.texture_button.pressed.connect(_on_play_button_pressed)
 
 func _on_play_button_pressed():
-	get_tree().change_scene_to_file("res://world/development_world.tscn")
+	get_tree().change_scene_to_file("res://world/game_world.tscn")
 
